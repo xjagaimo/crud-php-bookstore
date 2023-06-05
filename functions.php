02cc6b097d8d3a11c4cc8ec4d7982671638fa5ae
xjagaimo <nathanpk1902@gmail.com>
2023-06-05 10:25:11 +0700

    pdo

--- functions.php
+++ functions.php
@@ -6,7 +6,7 @@ function pdo_connect_pgsql() {
     $DATABASE_PASS = '190902';
     $DATABASE_NAME = 'bookstore';
     try {
-    	return new PDO('pgsql:host=' . $DATABASE_HOST . ';port=' . $DATABASE_PORT . ';dbname=' . $DATABASE_NAME . ';charset=utf8', $DATABASE_USER, $DATABASE_PASS);
+    	return new PDO('pgsql:host=' . $DATABASE_HOST . ';port=' . $DATABASE_PORT . ';dbname=' . $DATABASE_NAME, $DATABASE_USER, $DATABASE_PASS);
     } catch (PDOException $exception) {
     	// If there is an error with the connection, stop the script and display the error.
     	//exit('Failed to connect to database!');
