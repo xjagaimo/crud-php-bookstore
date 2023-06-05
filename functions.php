3e9e40ed4a1abb0c175bd1d171ff8c2585fb167d
xjagaimo <nathanpk1902@gmail.com>
2023-06-05 09:09:16 +0700

    exeption

--- functions.php
+++ functions.php
@@ -9,7 +9,8 @@ function pdo_connect_pgsql() {
     	return new PDO('postgresql:host=' . $DATABASE_HOST . ';port=' . $DATABASE_PORT . ';dbname=' . $DATABASE_NAME . ';charset=utf8', $DATABASE_USER, $DATABASE_PASS);
     } catch (PDOException $exception) {
     	// If there is an error with the connection, stop the script and display the error.
-    	exit('Failed to connect to database!');
+    	//exit('Failed to connect to database!');
+        exit($exception);
     }
 }
 function template_header($title) {
