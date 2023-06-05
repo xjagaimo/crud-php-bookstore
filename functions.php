23d279d3eb9af2e60de4d9a742b4cfd51f65967b
xjagaimo <nathanpk1902@gmail.com>
2023-06-05 08:09:55 +0700

    init

---
+++ functions.php
@@ -0,0 +1,41 @@
+<?php
+function pdo_connect_mysql() {
+    $DATABASE_HOST = 'localhost';
+    $DATABASE_PORT = '5432';
+    $DATABASE_USER = 'nathan';
+    $DATABASE_PASS = '190902';
+    $DATABASE_NAME = 'bookstore';
+    try {
+    	return new PDO('postgresql:host=' . $DATABASE_HOST . ';port=' . $DATABASE_PORT . ';dbname=' . $DATABASE_NAME . ';charset=utf8', $DATABASE_USER, $DATABASE_PASS);
+    } catch (PDOException $exception) {
+    	// If there is an error with the connection, stop the script and display the error.
+    	exit('Failed to connect to database!');
+    }
+}
+function template_header($title) {
+echo <<<EOT
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>$title</title>
+		<link href="style.css" rel="stylesheet" type="text/css">
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
+	</head>
+	<body>
+    <nav class="navtop">
+    	<div>
+    		<h1>Website Title</h1>
+            <a href="index.php"><i class="fas fa-home"></i>Home</a>
+    		<a href="read.php"><i class="fas fa-address-book"></i>Contacts</a>
+    	</div>
+    </nav>
+EOT;
+}
+function template_footer() {
+echo <<<EOT
+    </body>
+</html>
+EOT;
+}
+?>
